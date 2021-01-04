89d212ff074d33f4d42ca978b067fa32dd4814ee
Evgeniy Golovin <edgolovin@yandex.ru>
2021-01-05 00:36:05 +0700

    fix variables types

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
-		double ticketPrice = 20_011;
-		double milePrice = 20;
-		double milesEarned = ticketPrice / milePrice;
+		float ticketPrice = 20_011.33F;
+		int milePrice = 20;
+		int milesEarned = (int) ticketPrice / milePrice;
 		System.out.println(milesEarned);
 	}
 }
