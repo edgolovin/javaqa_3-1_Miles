b6f79e40631157a695b7939cf76678a59b91a48c
Evgeniy Golovin <edgolovin@yandex.ru>
2021-01-05 00:31:36 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		double ticketPrice = 20_011;
+		double milePrice = 20;
+		double milesEarned = ticketPrice / milePrice;
+		System.out.println(milesEarned);
+	}
+}
